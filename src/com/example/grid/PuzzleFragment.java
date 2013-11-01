637c4a3c8ac40370eae4917fa286b11b2562c339
Diego Iribarne <diego.iribarne@batanga.com>
2013-11-01 13:05:55 -0300

    cambia de click a slice.

--- src/com/example/grid/PuzzleFragment.java
+++ src/com/example/grid/PuzzleFragment.java
@@ -10,7 +10,9 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -18,16 +20,15 @@ import android.widget.GridView;
 import android.widget.TextView;
 
 public class PuzzleFragment extends Fragment {
-	private int columnas=3;
-	private int filas=3;
-	private int pedazos = columnas*filas;
+	private int columnas = 3;
+	private int filas = 3;
+	private int pedazos = columnas * filas;
 	private GridView gridview;
 	private ImageAdapter adapter;
 	int[] mapeadorTablero = new int[pedazos];
 	private TextView texto;
 	int espacioVacio;
 
-	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -49,7 +50,8 @@ public class PuzzleFragment extends Fragment {
 		texto = (TextView) getActivity().findViewById(R.id.text1);
 		Bitmap cover = BitmapFactory.decodeResource(getResources(),
 				R.drawable.cover);
-		adapter = new ImageAdapter(getActivity(), cover, filas, columnas, mapeadorTablero);
+		adapter = new ImageAdapter(getActivity(), cover, filas, columnas,
+				mapeadorTablero);
 		gridview.setVerticalScrollBarEnabled(false);
 		gridview.setNumColumns(columnas);
 		gridview.setAdapter(adapter);
@@ -61,13 +63,14 @@ public class PuzzleFragment extends Fragment {
 					intercambiar(position);
 					gridview.invalidateViews();
 				}
-				if(ganasteElJuego())
-					new FireMissilesDialogFragment().show(getFragmentManager(), getTag());
+				if (ganasteElJuego())
+					new FireMissilesDialogFragment().show(getFragmentManager(),
+							getTag());
 			}
 
 			private boolean ganasteElJuego() {
 				for (int i = 0; i < mapeadorTablero.length; i++) {
-					if(mapeadorTablero[i]!=i)
+					if (mapeadorTablero[i] != i)
 						return false;
 				}
 				return true;
@@ -84,7 +87,47 @@ public class PuzzleFragment extends Fragment {
 				return true;
 			}
 		});
+		gridview.setOnTouchListener(new OnTouchListener() {
 
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == event.ACTION_MOVE) {
+					int action = event.getActionMasked();
+					float currentXPosition = event.getX();
+					float currentYPosition = event.getY();
+					int position = gridview.pointToPosition(
+							(int) currentXPosition, (int) currentYPosition);
+					if (position != espacioVacio
+							&& detectarEspacioVacio(position)) {
+						intercambiar(position);
+						gridview.invalidateViews();
+					}
+					if (ganasteElJuego())
+						new FireMissilesDialogFragment().show(
+								getFragmentManager(), getTag());
+				}
+				return false;
+			}
+
+			private boolean ganasteElJuego() {
+				for (int i = 0; i < mapeadorTablero.length; i++) {
+					if (mapeadorTablero[i] != i)
+						return false;
+				}
+				return true;
+			}
+
+			private void intercambiar(int pos) {
+				int vacio = mapeadorTablero[espacioVacio];
+				mapeadorTablero[espacioVacio] = mapeadorTablero[pos];
+				espacioVacio = pos;
+				mapeadorTablero[pos] = vacio;
+			}
+
+			private boolean detectarEspacioVacio(int position) {
+				return true;
+			}
+		});
 	}
 
 	private void Mezclar() {
@@ -100,31 +143,35 @@ public class PuzzleFragment extends Fragment {
 				}
 			} while (existe);
 			mapeadorTablero[i] = random;
-			if (random == pedazos-1)
+			if (random == pedazos - 1)
 				espacioVacio = i;
 		}
 	}
 
 	@SuppressLint("ValidFragment")
 	public class FireMissilesDialogFragment extends DialogFragment {
-	    @Override
-	    public Dialog onCreateDialog(Bundle savedInstanceState) {
-	        // Use the Builder class for convenient dialog construction
-	        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-	        builder.setMessage("FELICIDADES")
-	               .setPositiveButton("JUGAR", new DialogInterface.OnClickListener() {
-	                   public void onClick(DialogInterface dialog, int id) {
-	                      Mezclar();
-	                      gridview.invalidateViews();
-	                   }
-	               })
-	               .setNegativeButton("CERRAR", new DialogInterface.OnClickListener() {
-	                   public void onClick(DialogInterface dialog, int id) {
-	                       
-	                   }
-	               });
-	        // Create the AlertDialog object and return it
-	        return builder.create();
-	    }
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the Builder class for convenient dialog construction
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setMessage("FELICIDADES")
+					.setPositiveButton("JUGAR",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									Mezclar();
+									gridview.invalidateViews();
+								}
+							})
+					.setNegativeButton("CERRAR",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+
+								}
+							});
+			// Create the AlertDialog object and return it
+			return builder.create();
+		}
 	}
 }
